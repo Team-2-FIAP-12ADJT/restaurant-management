8419bf0e341ba8872bed287cfeef1b8cf7568480
DeivisDigiHub <161029225+DeivisDigiHub@users.noreply.github.com>
2026-04-14 23:04:26 -0300

    feature: criada rotas de delete por id , e alterar senha do usuário

--- src/main/java/com/fiap/restaurant_management/dtos/UsersUpdatePassWordRequestDTO.java
+++ src/main/java/com/fiap/restaurant_management/dtos/UsersUpdatePassWordRequestDTO.java
@@ -1,7 +1,5 @@
 package com.fiap.restaurant_management.dtos;
 
-import com.fiap.restaurant_management.enums.RoleEnum;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
