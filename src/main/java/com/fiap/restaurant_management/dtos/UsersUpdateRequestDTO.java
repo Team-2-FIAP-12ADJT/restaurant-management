08733f95b4a7ad01d4e3b235a8bf2f1c9f3f7492
Ricardo Lima <ricardo.engp@gmail.com>
2026-05-03 23:50:39 -0300

    feat: adiciona suporte para hash de senhas e atualiza lógica de verificação de senha no serviço de usuários

--- src/main/java/com/fiap/restaurant_management/dtos/UsersUpdateRequestDTO.java
+++ src/main/java/com/fiap/restaurant_management/dtos/UsersUpdateRequestDTO.java
@@ -1,14 +1,13 @@
 package com.fiap.restaurant_management.dtos;
 
-import com.fiap.restaurant_management.enums.RoleEnum;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 
 public record UsersUpdateRequestDTO(
                 @Schema(example = "Gustavo") @Size(min = 1, message = "Name must not be blank when provided") String name,
                 @Schema(example = "gustavo123") @Size(min = 1, message = "Login must not be blank when provided") String login,
-                @Schema(example = "gustavo@email.com", format = "email") @Email(message = "Email should be valid") String email,
-                @Schema(example = "2", description = "Código do papel do usuário. 1=Owner, 2=Client") RoleEnum role) {
+                @Schema(example = "gustavo@email.com", format = "email") @Email(message = "Email should be valid") String email) {
+
         public UsersUpdateRequestDTO {
                 name = name != null ? name.trim() : null;
                 login = login != null ? login.trim() : null;
