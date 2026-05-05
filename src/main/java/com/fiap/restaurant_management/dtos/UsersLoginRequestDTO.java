b1ffe9433b3bc8f1b4b1c784fe9ad05df11f9f9e
Ricardo Lima <ricardo.engp@gmail.com>
2026-05-05 02:37:28 -0300

    feat: atualiza DTOs e contratos de autenticação para incluir exemplos e melhorar a documentação

--- src/main/java/com/fiap/restaurant_management/dtos/UsersLoginRequestDTO.java
+++ src/main/java/com/fiap/restaurant_management/dtos/UsersLoginRequestDTO.java
@@ -1,10 +1,11 @@
 package com.fiap.restaurant_management.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record UsersLoginRequestDTO(
-        @NotBlank(message = "Login is required") String login,
-        @NotBlank(message = "Password is required") String password
+        @Schema(example = "joao123") @NotBlank(message = "Login is required") String login,
+        @Schema(example = "Strong@123") @NotBlank(message = "Password is required") String password
 ) {
     public UsersLoginRequestDTO {
         login = login.trim();
